165c051c643d812665bca5524f03212047cfacec
vasylkucherak <vasil.kucherak@gmail.com>
2022-02-21 19:45:29 +0200

    v.0.7.0 (added SingleComicPage on site)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,7 +24,7 @@ const useMarvelService = () => {
         return res.data.results.map(_transformComics);
     }
 
-    const getComics = async (id) => {
+    const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
     }
@@ -47,13 +47,13 @@ const useMarvelService = () => {
             title: comics.title,
             description: comics.description || 'There is no description',
             thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
-            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            pageCount: comics.pageCount ? `${comics.pageCount} pages` : 'No information about the number of pages',
             language: comics.textObjects.language || 'en-us',
             price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComics}
+    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
 }
 
 export default useMarvelService;
